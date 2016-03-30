096d12ed22d3e6a3bd15567f4603a8ed0fea1837
Smbat Tumasyan <smbatt@davitg-mac.local>
2016-03-30 11:43:02 +0400

    Initial Commit

---
+++ MyStops/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyStops
+//
+//  Created by Smbat Tumasyan on 3/30/16.
+//  Copyright © 2016 EGS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
